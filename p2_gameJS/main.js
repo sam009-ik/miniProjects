f5eeefe1a394ae971ef56f9cf66381fe7d03a1a3
Samarth_N <138866449+sam009-ik@users.noreply.github.com>
2023-11-08 22:18:48 +0530

    changed Algorithm

--- p2_gameJS/main.js
+++ p2_gameJS/main.js
@@ -40,7 +40,17 @@ function handleButton(number) {
 }
 
 function compTurn() {
-    let computerNumber = 8 - userTurn[userTurn.length - 1]; // Computer's number by responding to user's number
+    let computerNumber;
+    if (currentSum >= 88) {
+        // If the total sum is 88 or more, choose the number that makes the total sum 92
+        computerNumber = 92 - currentSum;
+    } else if ((currentSum + 8) % 8 === 0) {
+        // If the total sum can be made a multiple of 8, choose the number that makes the sum 8
+        computerNumber = 8 - userTurn[userTurn.length - 1];
+    } else {
+        // Otherwise, choose the number that makes the total sum a multiple of 8
+        computerNumber = (8 - (currentSum % 8)) % 8;
+    }
     computerTurn.push(computerNumber);    // Add computer's number to the log
     currentSum += computerNumber; 
     sumEl.textContent = `SUM: ${currentSum}`; // Update sum in the DOM
